ecaf6c5ee79e34b28ec5e5ea027df3f389a8bdf7
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2023-11-04 14:58:54 +0100

    A main module has been updated.

--- src/Incertus.jl
+++ src/Incertus.jl
@@ -6,14 +6,22 @@ using Plots
 using ProgressMeter
 using Random
 using Roots
-    
+ 
+abstract type CompleteRandomization end
 abstract type RestrictedRandomization end
 
 include("abcd.jl")
+include("crd.jl")
 include("ebcd.jl")
 
+include("utils.jl")
+
+export CompleteRandomization
+export CRD
 export RestrictedRandomization
-export EBCD, ABCD
+export ABCD, CRD, EBCD
+
 export allocation_prb
+export set_label
 
 end # module Incertus
