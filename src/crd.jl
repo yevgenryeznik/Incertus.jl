30bbd422006ed4b0a98958e59aa2a40412270433
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2023-11-04 14:29:04 +0100

    CRD has been implemnted.

---
+++ src/crd.jl
@@ -0,0 +1,28 @@
+"""A type of randomization, representing _**C**_ompletely _**R**_andomized _**D**_esign (_**CRD**_).
+"""
+struct CRD 
+    target::Vector{<:Number}
+    CRD(target::Vector{<:Number}) = new(simplify(target))
+end
+CRD() = CRD([1, 1])
+
+
+# function calculates allocation probability for Complete Randomization
+"""Function calculates allocation probabilities for CRD.
+
+# Arguments
+- `rnd::CRD`: an object, representing Complete Randomization.
+"""
+function allocation_prb(rnd::CRD)
+    # target alloaction
+    w = rnd.target    
+
+    # probability(ies) of treatment assignment
+    if allequal(w) && length(w) == 2
+        prb = 0.5
+    else
+        prb = w/sum(w)
+    end
+
+    return prb
+end
