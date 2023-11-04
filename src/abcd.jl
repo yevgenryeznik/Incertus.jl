cdb83a0cf5538192c476199e8853a65d43572088
Yevgen Ryeznik <yevgen.ryeznik@gmail.com>
2023-11-04 13:52:08 +0100

    ABCD has been implemented.

--- src/abcd.jl
+++ src/abcd.jl
@@ -15,3 +15,27 @@ struct ABCD <: RestrictedRandomization
     end
 end
 ABCD(a::Number) = ABCD([1, 1], a)
+
+
+# function calculates allocation probability for Adjustable Biased Coin Design
+"""Function calculates allocation probabilities for ABCD, given treatment numbers.
+
+# Arguments
+- `rnd::ABCD`: an object, representing Adjustable Biased Coin Design.
+- `N::Vector{Int64}`: a vector of current treatment numbers.
+"""
+function allocation_prb(rnd::ABCD, N::Vector{Int64})
+    # allocation function proposed by A. Baldi Antognini and A. Giovagnoli
+    F(x, a) = abs(x)^a/(abs(x)^a + 1)
+
+    # parameter of the randomization procedure
+    a = rnd.a
+
+    # current treatment imbalance
+    d = N[1] - N[2]
+
+    # allocation probability, given current imbalance
+    prb = abs(d) <= 1 ? 0.5 : (d < -1 ? F(d, a) : 1-F(d, a))    
+
+    return prb
+end
