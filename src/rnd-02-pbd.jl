0d3f65c7e46322c68d0b8d24e3474f09d201e1a8
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:30:58 +0200

    PBD interface & implementation have been updated.

--- src/rnd-02-pbd.jl
+++ src/rnd-02-pbd.jl
@@ -10,15 +10,15 @@ An output of both commands is an instance of `PBD`.
 """
 struct PBD <: RestrictedRandomization 
     target::Vector{Int64}
-    λ::Int64
+    parameter::Int64
 
-    function PBD(target::Vector{Int64}, λ::Int64) 
+    function PBD(target::Vector{Int64}, parameter::Int64) 
         @assert eltype(target) == Int64 "The procedure isn't implemented for non-integer target allocation";
 
-        return new(simplify(target), λ)
+        return new(simplify(target), parameter)
     end
 end
-PBD(λ::Int64) = PBD([1, 1], λ)
+PBD(parameter::Int64) = PBD([1, 1], parameter)
 
 
 """Function calculates allocation probabilities for PBD, given treatment numbers.
@@ -34,7 +34,7 @@ function allocation_prb(rnd::PBD, N::Vector{Int64})
     w = rnd.target
 
     # parameter of the randomization procedure
-    λ = rnd.λ
+    λ = rnd.parameter
 
     # a block size
     bs = λ*sum(w)
@@ -45,20 +45,8 @@ function allocation_prb(rnd::PBD, N::Vector{Int64})
     # number of complete blocks among the previous assignments
     k = floor((j-1)/bs)
 
-    # number of treatments
-    ntrt = length(w)
-
-    # if it is a randomization, targeting 1:1 allocation
-    if ntrt == 2 && allequal(w)
-        # calculating probability of treatment assignment
-        prb = (0.5*bs*(1 + k) - N[1])/(bs*(1 + k)-(j-1))
-
-    # otherwise, it is a randomization, targeting allocation w 
-    # (unequal in a two-arm trial or equal/unequal in a multi-arm trial)   
-    else
-        # vector of probabilities of treatment assignments
-        prb = [(λ*w[i]*(1 + k) - N[i])/(bs*(1 + k) - (j-1)) for i in eachindex(w)]
-    end  
+    # probabilities of treatments' assignments
+    prb = [(λ*w[i]*(1 + k) - N[i])/(bs*(1 + k) - (j-1)) for i in eachindex(w)]
 
     return prb
 end
