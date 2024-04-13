a2fddb156f47a6c60d89657c7e2dd29f9c10e50f
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:35:49 +0200

    `set_lavbel()` has been renamed to `label()`; implemenation has been modified.

--- src/utils.jl
+++ src/utils.jl
@@ -6,7 +6,7 @@ end
 
 
 # function represents a `Number` as a string
-as_string(param::Number) = @match param begin
+as_string(parameter::Number) = @match parameter begin
     p::Rational{Int64} => "$(numerator(p))/$(denominator(p))"
     p::Float64         => "$(round(p, digits = 2))" 
     p::Int64           => "$(p)"
@@ -14,7 +14,6 @@ end
 
 
 """Function sets a label for a randomization procedure.
-
 An example:
 ```julia-repl
 julia> w = [1, 2, 3, 4]
@@ -27,23 +26,30 @@ julia> w = [1, 2, 3, 4]
 julia> dlr_proc = DLR(w, 2)
 DLR(2): restricted randomization procedure, targeting 1:2:3:4 allocation in 4-arm trial.
 
-julia> label = set_label(dlr_proc)
+julia> label(dlr_proc)
 "DLR(2)"
 ```
 """
-set_label(rnd::Union{CompleteRandomization, RestrictedRandomization}) = @match rnd begin
-    r::CRD    => "CRD"
-    r::TBD    => "TBD"
-    r::PBD    => "PBD($(as_string(r.λ)))"
-    r::RAND   => "Rand"
-    r::EBCD   => "EBCD($(as_string(r.p)))"
-    r::ABCD   => "ABCD($(as_string(r.a)))"
-    r::GBCD   => "GBCD($(as_string(r.γ)))"
-    r::BSD    => "BSD($(as_string(r.mti)))"
-    r::BCDWIT => "BCDWIT($(as_string(r.p)), $(as_string(r.mti)))"
-    r::BUD    => "BUD($(as_string(r.λ)))"
-    r::EUD    => "EUD($(as_string(r.mti)))"
-    r::BBCD   => "BBCD($(as_string(r.γ)))"
+function label(rnd::Union{CompleteRandomization, RestrictedRandomization}) 
+    procedure = "$(typeof(rnd))"
+    fields = fieldnames(typeof(rnd))
+    values = [as_string(getfield(rnd, f)) for f in fields if f != :target]
+    
+    return procedure * "(" * join(values, ", ") * ")"
+
+    #@match rnd begin
+    #    r::CRD    => "CRD"
+    #    r::TBD    => "TBD"
+    #r::PBD    => "PBD($(as_string(r.λ)))"
+    #r::RAND   => "Rand"
+    #r::EBCD   => "EBCD($(as_string(r.p)))"
+    #r::ABCD   => "ABCD($(as_string(r.a)))"
+    #r::GBCD   => "GBCD($(as_string(r.γ)))"
+    #r::BSD    => "BSD($(as_string(r.mti)))"
+    #r::BCDWIT => "BCDWIT($(as_string(r.p)), $(as_string(r.mti)))"
+    #r::BUD    => "BUD($(as_string(r.λ)))"
+    #r::EUD    => "EUD($(as_string(r.mti)))"
+    #r::BBCD   => "BBCD($(as_string(r.γ)))"
     #r::MWUD   => "MWUD($(as_string(r.α)))"
     #r::DLR    => "DLR($(as_string(r.a)))"
     #r::DBCD   => "DBCD($(as_string(r.γ)))"
@@ -57,7 +63,7 @@ function Base.show(io::IO, rnd::Union{CompleteRandomization, RestrictedRandomiza
     # it is assumed that any object of `CRD` or `RestrictedRandomization` subtype
     # has the following field: `target`
     target = rnd.target
-    label = set_label(rnd)
+    lab = label(rnd)
 
     tar = join(target, ":")  # String representation of the target allocation ratio
     ntrt = length(target)    # Calculating number of treatment arms
@@ -66,7 +72,9 @@ function Base.show(io::IO, rnd::Union{CompleteRandomization, RestrictedRandomiza
         _      => "restricted"
     end
 
-    description = "$(label): $(class) randomization procedure, targeting $(tar) allocation in $(ntrt)-arm trial."
+    description = "$(lab): $(class) randomization procedure, targeting $(tar) allocation in $(ntrt)-arm trial."
     println(io, description)
+
+    return nothing
 end
 
