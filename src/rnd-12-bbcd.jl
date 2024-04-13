4af57d25091a03323f72b40d5e1a1dab1ce0913f
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:34:19 +0200

    BBCD interface & implementation have been updated.

--- src/rnd-12-bbcd.jl
+++ src/rnd-12-bbcd.jl
@@ -7,24 +7,24 @@ An output of the command is an isntance of BBCD.
 """
 struct BBCD <: RestrictedRandomization
     target::Vector{Int64}
-    γ::Number
-    nsbj::Int64
+    parameter1::Number # parameter γ
+    parameter2::Int64  # sample size
 
 
-    function BBCD(target::Vector{Int64}, γ::Number, nsbj::Int64) 
+    function BBCD(target::Vector{Int64}, parameter1::Number, parameter2::Int64) 
         # getting number of treatments
         ntrt = length(target)
     
         @assert ntrt == 2 "The procedure isn't implemented for multi-arm trials!";
         @assert allequal(target) "The procedure isn't implemented for unequal allocation!";
         @assert eltype(target) == Int64 "The procedure isn't implemented for non-integer target allocation!";
-        @assert γ > 0 "The procedure's parameter, `γ`, must be positive!";    
+        @assert parameter1 > 0 "The procedure's parameter, `γ`, must be positive!";    
         
-        return new(simplify(target), γ, nsbj)
+        return new(simplify(target), parameter1, parameter2)
     end
     
 end
-BBCD(γ::Number, nsbj::Int64) = BBCD([1, 1], γ, nsbj)
+BBCD(parameter1::Number, parameter2::Int64) = BBCD([1, 1], parameter1, parameter2)
 
 """Function calculates allocation probabilities for BBCD, given treatment numbers.
 # Call
@@ -36,14 +36,15 @@ BBCD(γ::Number, nsbj::Int64) = BBCD([1, 1], γ, nsbj)
 """
 function allocation_prb(rnd::BBCD, N::Vector{Int64})
     # parameters of the randomization procedure (BBCD)
-    γ = rnd.γ
-    n = rnd.nsbj
+    γ = rnd.parameter1
+    n = rnd.parameter2
 
     # current subject's ID
     j = N[1] + N[2]
 
-    # probability of treatment assignment
-    ϕ = j+1 == 1 ? 0.5 : (j+1 == 2 ? Int(N[1] == 0) : (n + N[2]/N[1])^(1/γ)/((n + N[2]/N[1])^(1/γ) + (n + N[1]/N[2])^(1/γ)))
-    
-    return ϕ 
+    # probabilities of treatments' assignments
+    P = j+1 == 1 ? 0.5 : (j+1 == 2 ? Int(N[1] == 0) : (n + N[2]/N[1])^(1/γ)/((n + N[2]/N[1])^(1/γ) + (n + N[1]/N[2])^(1/γ)))
+    prb = [P, 1-P]
+
+    return prb 
 end
