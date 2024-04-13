4fa0aa5a0f1f075401bf2a5cbdcbd86b5300ba4a
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:33:13 +0200

    GBCD interface & implementation have been updated.

--- src/rnd-07-gbcd.jl
+++ src/rnd-07-gbcd.jl
@@ -6,26 +6,20 @@ targeting `1:1` allocation.
 An output of the command is an isntance of GBCD."""
 struct GBCD <: RestrictedRandomization 
     target::Vector{Int64}
-    γ::Number
+    parameter::Number
 
-    function GBCD(target::Vector{Int64}, γ::Number)
+    function GBCD(target::Vector{Int64}, parameter::Number)
         # getting number of treatments
         ntrt = length(target)
         
-        @assert γ >= 0 "The procedures parameter, `γ`, must be non-negative";
+        @assert parameter >= 0 "The procedures parameter, `γ`, must be non-negative";
         @assert ntrt == 2 "The procedure isn't implemented for multi-arm trials";
         @assert allequal(target) "The procedure isn't implemented for unequal allocation";
 
-        if ntrt > 2 
-            throw(NotImplementedForMultiArmTrial())
-        elseif !allequal(target) 
-            throw(NotImplementedForUnequalAllocation())
-        else
-            return new([1, 1], γ)
-        end
+        return new(target, parameter)
     end
 end
-GBCD(γ::Number) = GBCD([1, 1], γ)
+GBCD(parameter::Number) = GBCD([1, 1], parameter)
 
 
 """Function calculates allocation probabilities for GBCD, given treatment numbers.
@@ -38,13 +32,14 @@ GBCD(γ::Number) = GBCD([1, 1], γ)
 """
 function allocation_prb(rnd::GBCD, N::Vector{Int64})
     # getting a parameter value of the randomization procedure
-    γ = rnd.γ
+    γ = rnd.parameter
 
     # indicating a current allocation step (current subject's ID)
     j = sum(N) + 1
 
-    # calculating probability of treatment asssgnment
-    prb = j == 1 ? 0.5 : N2^γ/(N1^γ + N2^γ) 
+    # probabilities of treatments' assignments
+    P = j == 1 ? 0.5 : N[2]^γ/(N[1]^γ + N[2]^γ) 
+    prb = [P, 1-P]
 
     return prb
 end
