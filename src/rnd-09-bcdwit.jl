a5bdf8281aa1924d1718fd553f3db41c87fda939
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:33:39 +0200

    BCDWIT interface & implementation have been updated.

--- src/rnd-09-bcdwit.jl
+++ src/rnd-09-bcdwit.jl
@@ -8,23 +8,22 @@ An output of the command is an isntance of BCDWIT.
 """
 struct BCDWIT <: RestrictedRandomization
     target::Vector{Int64}
-    p::Number
-    mti::Int64
+    parameter1::Number # probability of a biased coin
+    parameter2::Int64  # MTI parameter
 
-    function BCDWIT(target::Vector{Int64}, p::Number, mti::Int64)
+    function BCDWIT(target::Vector{Int64}, parameter1::Number, parameter2::Int64)
         # getting number of treatments
         ntrt = length(target)
         
-        @assert mti > 0 "The procedures parameter, `mti`, must be positive";
-        @assert 0.5 <= p <= 1 "The procedures parameter, `p`, must be between 0.5 and 1";
+        @assert parameter2 > 0 "The procedure's parameter, `mti`, must be positive";
+        @assert 0.5 <= parameter1 <= 1 "The procedure's parameter, `p`, must be between 0.5 and 1";
         @assert ntrt == 2 "The procedure isn't implemented for multi-arm trials";
         @assert allequal(target) "The procedure isn't implemented for unequal allocation";
 
-
-        return new([1, 1], p, mti)
+        return new(target, parameter1, parameter2)
     end
 end
-BCDWIT(p::Number, mti::Int64) = BCDWIT([1, 1], p, mti)
+BCDWIT(parameter1::Number, parameter2::Int64) = BCDWIT([1, 1], parameter1, parameter2)
 
 
 """Function calculates allocation probabilities for BCDWIT, given treatment numbers.
@@ -37,16 +36,17 @@ BCDWIT(p::Number, mti::Int64) = BCDWIT([1, 1], p, mti)
 """
 function allocation_prb(rnd::BCDWIT, N::Vector{Int64})
     # getting a probability of a biased coin
-    p = Float64(rnd.p)
+    p = Float64(rnd.parameter1)
 
     # getting MTI parameter
-    mti = rnd.mti  
+    mti = rnd.parameter2  
 
     # calculating current treatment imbalance
     d = N[1] - N[2]
  
-    # calculating probability of tretament asssgnment, given imbalance
-    prb = abs(d) < mti ? (d == 0 ? 0.5 : (d < 0 ? p : 1-p)) : (d == mti ? 0 : 1)  
+    # probabilities of treatments' assignments
+    P = abs(d) < mti ? (d == 0 ? 0.5 : (d < 0 ? p : 1-p)) : (d == mti ? 0 : 1)  
+    prb = [P, 1-P]
 
     return prb
 end
