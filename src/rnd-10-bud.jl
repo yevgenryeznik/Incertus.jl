bf436aa4d37c8b9fe9ee7dc41a64aba2dc257291
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-04-13 13:33:51 +0200

    BUD interface & implementation have been updated.

--- src/rnd-10-bud.jl
+++ src/rnd-10-bud.jl
@@ -10,17 +10,17 @@ An output of both commands is an instance of `BUD`.
 """
 struct BUD <: RestrictedRandomization
     target::Vector{Int64}
-    λ::Int64
+    parameter::Int64
 
-    function BUD(target::Vector{Int64}, λ::Int64) 
+    function BUD(target::Vector{Int64}, parameter::Int64) 
         @assert eltype(target) == Int64 "The procedure isn't implemented for non-integer target allocation!";
-        @assert λ > 0 "The procedure's parameter, `λ`, must be positive!";    
+        @assert parameter > 0 "The procedure's parameter, `λ`, must be positive!";    
         
-        return new(simplify(target), λ)
+        return new(simplify(target), parameter)
     end
     
 end
-BUD(λ::Int64) = BUD([1, 1], λ)
+BUD(parameter::Int64) = BUD([1, 1], parameter)
 
 """Function calculates allocation probabilities for BUD, given treatment numbers.
 # Call
@@ -32,27 +32,20 @@ BUD(λ::Int64) = BUD([1, 1], λ)
 """
 function allocation_prb(rnd::BUD, N::Vector{Int64})
     # parameter of the randomization procedure (BUD)
-    λ = rnd.λ
+    λ = rnd.parameter
 
     # target allocation
     w = rnd.target
     W = sum(w)
     
-    # number of treatments
-    ntrt = length(w)
-
     # current subject's ID
     j = sum(N) + 1
 
-    if ntrt == 2
-        prb = (λ + minimum(N) - N[1])/(2*(λ + minimum(N)) - (j-1))
-    else
-        # the number of minimal balanced sets in previous assignments
-        k = minimum([floor(N[i]/w[i]) for i in eachindex(w)])
-    
-        # probabilities of tretament assignments
-        [(w[i]*(λ + k) - N[i])/(W*(λ + k) - (j-1)) for i in eachindex(w)]
-    end
+    # the number of minimal balanced sets in previous assignments
+    k = minimum([floor(N[i]/w[i]) for i in eachindex(w)])
+
+    # probabilities of treatments' assignments
+    prb = [(w[i]*(λ + k) - N[i])/(W*(λ + k) - (j-1)) for i in eachindex(w)]
 
     return prb 
 end
